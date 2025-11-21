af0915be2bc98a9a8055b9523c489c8d81e659ae
W3i537h <maatro_1001@hotmail.com>
2025-11-21 08:49:57 +0100

    Update KillActiveProcess.sh

--- config/hypr/scripts/KillActiveProcess.sh
+++ config/hypr/scripts/KillActiveProcess.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-
-# Copied from Discord post. Thanks to @Zorg
-
 
 # Get id of an active window
 active_pid=$(hyprctl activewindow | grep -o 'pid: [0-9]*' | awk '{print $2}')
